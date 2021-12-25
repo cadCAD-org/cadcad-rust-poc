1d64c74e0bcb6cd0544c67492e686e4ecebe39bf
Gokhan Simsek <g.simsek@paralect.com>
2021-12-25 12:23:11 +0300

    Refactoring

--- test_pyo3.py
+++ test_pyo3.py
@@ -2,22 +2,25 @@ import cadcad_rs, random
 
 ##
 sim_config = {
-    'T': 7,  # timesteps
+    'T': 10,  # timesteps
     'N': 1,   # times the simulation will be run (Monte Carlo runs)
 }
 
+##
 init_state = {
     'preys'    : 2000,
     'predators':  200.0,
 }
 
-# Policies
+## Params
+MAX_PREYS = 3000
+
+## Policies
 def prey_change_normal_conditions(state, y):
-    max_preys = 3000
     preys =  state['preys']
-    # Assuming: preys_change goes down with every iteration since 
-    # natural resources are limits it to max_preys 
-    preys_change = random.randint(0, max_preys-preys) if preys < max_preys else 0
+    # Assuming: preys_change goes down with every iteration since
+    # natural resources limits the number of preys to MAX_PREYS 
+    preys_change = random.randint(0, MAX_PREYS-preys) if preys < MAX_PREYS else 0
     return ( "preys_change", preys_change )
 
 def predator_change_normal_conditions(state, y):
@@ -28,8 +31,10 @@ policies = [
     predator_change_normal_conditions
 ]
 
+##
 print_trajectory = bool(1)
 
+##
 cadcad_rs.run_simulation_rs(
   "config from python",
   sim_config,
