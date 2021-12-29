7bbd90694378562a97cfbff5f32adb12415a4a73
Gokhan Simsek <g.simsek@paralect.com>
2021-12-29 13:14:34 +0300

    Minor change

--- test_pyo3.py
+++ test_pyo3.py
@@ -1,8 +1,8 @@
-import cadcad_rs, random
+import cadcad_rs, random, time
 
 ##
 sim_config = {
-    'T': 10,  # timesteps
+    'T': 100000,  # timesteps
     'N': 1,   # times the simulation will be run (Monte Carlo runs)
 }
 
@@ -35,6 +35,8 @@ policies = [
 ]
 
 ##
+start = time.process_time()
+##
 cadcad_rs.run_simulation_rs(
   "config from python",
   sim_config,
@@ -42,3 +44,6 @@ cadcad_rs.run_simulation_rs(
   policies,
   print_trajectory
 )
+##
+end = time.process_time()
+print(end - start, "sec(s)")
