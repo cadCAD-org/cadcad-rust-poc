eefcedf8e5382fb212b28e7ad4cbe50f929dc0c1
Gokhan Simsek <g.simsek@paralect.com>
2022-01-25 15:35:38 +0300

    Add files

---
+++ cprofile_used/config_prey_predator.py
@@ -0,0 +1,71 @@
+## Info:
+## This file is a reference (and dummy) user config to use with performance tests
+import cProfile
+
+import cadcad_rs, random, time
+
+##
+sim_config = {
+    'T': 100_000,  # timesteps
+    # 'T': 10,  # timesteps
+    'N': 1,   # times the simulation will be run (Monte Carlo runs)
+}
+
+##
+print_trajectory = bool(0)
+
+##
+init_state = {
+    'preys'    : 2000,
+    'predators':  200.0,
+}
+
+## Params
+MAX_PREYS = 3000
+
+## Policies
+def prey_change_normal_conditions(state, y):
+    preys =  state['preys']
+    # Assuming: preys_change goes down with every iteration since
+    # natural resources limits the number of preys to MAX_PREYS 
+    preys_change = random.randint(0, MAX_PREYS-preys) if preys < MAX_PREYS else 0
+    return ( "preys_change", preys_change )
+
+def predator_change_normal_conditions(state, y):
+    return ( "predators_change", random.uniform(-10.0, 10.0) )   
+
+policies = [
+    prey_change_normal_conditions, 
+    predator_change_normal_conditions
+]
+
+# SUFS/Mechanisms
+def update_prey(state, signals):
+    preys = state['preys'] + signals['preys_change']
+    return ('preys', preys)
+
+def update_predator(state, signals):
+    predators = state['predators'] + signals['predators_change']
+    return ('predators', predators)
+
+state_update_fns = [
+    update_prey,
+    update_predator
+]
+
+def main():
+    cadcad_rs.run_simulation(
+        "config from python",
+        sim_config,
+        init_state,
+        policies,
+        state_update_fns,
+        print_trajectory
+    )
+
+# if __name__ == '__main__':
+#     cProfile.run('main()')
+start = time.process_time()
+main()
+end = time.process_time()
+print("\nElapsed Time:", end - start, "sec(s)")
